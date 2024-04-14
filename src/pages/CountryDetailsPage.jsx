ac0f801e9357da86f7fbcbd18b6e193c50b4e568
RaquelMelow <raquelmelo949@gmail.com>
2024-04-14 13:53:32 +0100

    Done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,58 @@
-function CountryDetails() {}
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import countriesData from "../data/countries.json";
+import "./CountryDetailsPage.css";
 
-export default CountryDetails;
+const CountryDetailsPage = () => {
+    const { countryId } = useParams();
+    const [countryDetails, setCountryDetails] = useState(null);
+
+    useEffect(() => {
+        const findItemByAlpha3Code = (code) => {
+            console.log(code);
+            return countriesData.find((item) => item.alpha3Code === code);
+        };
+
+        const country = findItemByAlpha3Code(countryId);
+        setCountryDetails(country);
+    }, [countryId]);
+
+    if (!countryDetails) {
+        return <div>Loading...</div>;
+    }
+
+    const { alpha2Code, name, capital, area, borders } = countryDetails;
+
+    return (
+        <div className="country-card">
+            <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${alpha2Code.toLowerCase()}.png`}
+                alt={name.official}
+                className="country-flag"
+            />
+            <div className="country-content">
+                <h1>{name.official}</h1>
+                <p>
+                    <strong>Capital:</strong> {capital}
+                </p>
+                <p>
+                    <strong>Area:</strong> {area}km
+                </p>
+                <p>
+                    <strong>Borders:</strong>
+                </p>
+                <ul className="country-borders">
+                    {borders && borders.length > 0 ? (
+                        borders.map((border, index) => (
+                            <li key={index}>{border}</li>
+                        ))
+                    ) : (
+                        <li>No borders</li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default CountryDetailsPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,27 @@
-function HomePage() {}
+import React, { useEffect, useState } from "react";
+import countriesData from "../data/countries.json";
+import { Link } from "react-router-dom";
+import "./HomePage.css"
+
+const HomePage = () => {
+  const [countries, setCountries] = useState(countriesData);
+
+  return (
+    <div className="container">
+      <h1>WikiCountries: Your Guide to the World</h1>
+      <div className="list-group">
+        {countries.map((country) => (
+          <Link to={`/${country.alpha3Code}`} key={country._id} className="list-item">
+            <img
+              src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+              alt={`flag-of-${country.name.common}`}
+            />
+            <p>{country.name.common}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
